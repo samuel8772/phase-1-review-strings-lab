a65bdd1e8415f4112eb4eb9e312eedf853065a51
samuel8772 <kamauskk005@gmail.com>
2025-03-20 11:33:55 +0300

    Update index.js and results.json

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Alice"; // Replace with any non-empty string
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
